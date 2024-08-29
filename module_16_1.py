313fac91dbdb131520933a7af6f22175c475b0a8
Dmitry <kadzzi@gmail.com>
2024-08-29 18:21:14 +0300

    added module_16_1.py

---
+++ module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Главная страница"}
+
+
+@app.get("/user/admin")
+async def admin() -> dict:
+    return {"message": "Вы вошли как администратор"}
+
+
+@app.get("/user/{user_id}")
+async def any_user(user_id: str) -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+@app.get("/user")
+async def user_info(username: str, age: int) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
